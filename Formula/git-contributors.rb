54c06896a91b0a1bf8cf4ff24586456681655424
basictofu <none>
2026-03-06 16:09:28 -0600

    Remove comments

--- Formula/git-contributors.rb
+++ Formula/git-contributors.rb
@@ -1,6 +1,5 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class GitContributors < Formula
   desc "Show commit histograms in the terminal"
   homepage "https://github.com/basictofu/git-contributors"
@@ -10,26 +9,11 @@ class GitContributors < Formula
 
   depends_on "rust" => :build
 
-  # Additional dependency
-  # resource "" do
-  #   url ""
-  #   sha256 ""
-  # end
-
   def install
     system "cargo", "install", *std_cargo_args
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test git-contributors`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
     system "git", "contributors", "-h"
   end
 end
